d507b861978779e1acea9b8188f2a7579637a850
nidhiG2610 <nidhigajera.mobio@gmail.com>
2026-02-23 15:40:41 -0500

    Added default roles

--- backend/database/migrations/2026_02_11_223003_create_roles_table.php
+++ backend/database/migrations/2026_02_11_223003_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -17,6 +18,12 @@ return new class extends Migration
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
+
+        // insert 2 default roles
+        DB::table('roles')->insert([
+            ['name' => 'admin', 'active' => true],
+            ['name' => 'user', 'active' => true],
+        ]);
     }
 
     /**
