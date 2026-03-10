b1c64f253cf21d5db1cd3e8c15ab81730c6b12b6
nidhiG2610 <nidhigajera.mobio@gmail.com>
2026-03-10 19:55:40 -0400

    Updating Db name

--- backend/config/database.php
+++ backend/config/database.php
@@ -88,7 +88,7 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'analyticsdashboard'),
+            'database' => env('DB_DATABASE', 'analytics-dashboard'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
